090c6ee34923b83e2df49f04026aec32f535cfdb
yuta lab <nkjjmyut0511@gmmail.com>
2022-11-21 15:08:12 +0900

    login form (not worked)

--- app/app.py
+++ app/app.py
@@ -1,12 +1,43 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template, redirect, url_for, session
+import pyrebase
+import json, os
+
+with open("firebaseConfig.json") as f:
+    firebaseConfig = json.loads(f.read())
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
 
 
-@app.route('/') 
-def hello_world():
-    return 'Hello my project!' 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html",msg="")
+
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        user = auth.sign_in_with_email_and_password(email, password)
+        session['usr'] = email
+        return redirect(url_for('index'))
+    except:
+        return render_template("login.html", msg="メールアドレスまたはパスワードが間違っています。")
+
+@app.route("/", methods=['GET'])
+def index():
+    usr = session.get('usr')
+    if usr == None:
+        return redirect(url_for('login'))
+    return render_template("index.html", usr=usr)
+
+@app.route('/logout')
+def logout():
+    del session['usr']
+    return redirect(url_for('login'))
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.debug = True
+    app.run(port=8888)
